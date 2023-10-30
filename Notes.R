23d9597a0a88f0573258a1e734206cf69c1e695f
kvsathish <karthiksathish2025@u.northwestern.edu>
2023-10-29 22:57:42 -0500

    finished the progress memo

---
+++ Notes.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(naniar)
+
+allgames_stats <- read_csv("data/allgames_stats.csv")
+
+allgames_stats
+
+class_count <- sapply(allgames_stats, class)
+num_categorical <- sum(class_count %in% c("factor", "character"))
+
+cat("Number of categorical variables:", num_categorical)
+
+allgames_stats |> 
+  naniar::miss_var_summary() |> 
+  print(n = 29)
