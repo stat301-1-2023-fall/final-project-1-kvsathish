f7205d8ce9ec28a2868d5a5558ecb278a0a6a697
kvsathish <karthiksathish2025@u.northwestern.edu>
2023-11-22 17:36:42 -0600

    finished first szn for kobe

--- Notes.R
+++ Notes.R
@@ -18,3 +18,36 @@ allgames_stats |>
 ## Progress Memo 2 ----
 
 devtools::install_github("abresler/nbastatR")
+
+## Kobe first year ----
+kobe_first_year <- allgames_stats %>%
+  filter(Date >= as.Date("1996-11-03") & Date <= as.Date("1997-04-20")) |> 
+  filter(Player == "Kobe Bryant") |> 
+  mutate(Score = (PTS + AST + TRB + STL + BLK) / as.numeric(MP) * 10000) |> 
+  select(Player, Date, Score) 
+
+ggplot(kobe_first_year, aes(Score)) +
+  geom_boxplot() +
+  labs(title = "Distribution of Calculated Score", subtitle = "For Kobe's First Season",
+       x = "Score", y = NULL)
+
+
+
+ggplot(kobe_first_year, aes(Date, Score)) +
+  geom_point() +
+  geom_smooth() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Calculated Score by Date for Kobe Bryant")
+
+kobe_first_year <- head(kobe_first_year, 10)
+kable(kobe_first_year)
+
+kobe_fy_mean <- allgames_stats %>%
+  filter(Date >= as.Date("1996-11-03") & Date <= as.Date("1997-04-20")) |> 
+  filter(Player == "Kobe Bryant") |> 
+  mutate(Score = (PTS + AST + TRB + STL + BLK) / as.numeric(MP) * 10000) |> 
+  summarize(Mean_Score = mean(Score))
+
+kable(kobe_fy_mean)
+
+
