8a7de73bed3498ba9cd89d307fcb247ee3929173
kvsathish <karthiksathish2025@u.northwestern.edu>
2023-11-22 18:12:46 -0600

    lebron first season done

--- Notes.R
+++ Notes.R
@@ -49,7 +49,7 @@ ggsave(
   units = "in"
 )
 
-kobe_first_year <- head(kobe_first_year, 10)
+kobe_first_year <- head(kobe_first_year, 5)
 kable(kobe_first_year)
 
 kobe_fy_mean <- allgames_stats %>%
@@ -59,5 +59,46 @@ kobe_fy_mean <- allgames_stats %>%
   summarize(Mean_Score = mean(Score))
 
 kable(kobe_fy_mean)
+
+## Lebron first year ----
+lebron_first_year <- allgames_stats %>%
+  filter(Date >= as.Date("2003-10-29") & Date <= as.Date("2004-04-14")) |> 
+  filter(Player == "Lebron James") |> 
+  mutate(Score = (PTS + AST + TRB + STL + BLK) / as.numeric(MP) * 10000) |> 
+  select(Player, Date, Score) 
+
+lebron_score1 <- ggplot(lebron_first_year, aes(Score)) +
+  geom_boxplot() +
+  labs(title = "Distribution of Calculated Score", subtitle = "For Lebron's First Season",
+       x = "Score", y = NULL)
+
+ggsave(
+  filename = "plots/lebron_score1.png",
+  plot = lebron_score1,
+  units = "in"
+)
+
+lebron_sbd1 <- ggplot(lebron_first_year, aes(Date, Score)) +
+  geom_point() +
+  geom_smooth() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Calculated Score Over Lebron James' First Season")
+
+ggsave(
+  filename = "plots/lebron_sbd1.png",
+  plot = lebron_sbd1,
+  units = "in"
+)
+
+lebron_first_year <- head(lebron_first_year, 5)
+kable(lebron_first_year)
+
+lebron_fy_mean <- allgames_stats %>%
+  filter(Date >= as.Date("2003-10-29") & Date <= as.Date("2004-04-14")) |> 
+  filter(Player == "Lebron James") |> 
+  mutate(Score = (PTS + AST + TRB + STL + BLK) / as.numeric(MP) * 10000) |> 
+  summarize(Mean_Score = mean(Score))
+
+kable(lebron_fy_mean)
 
 
