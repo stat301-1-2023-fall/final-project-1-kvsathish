fc1601a6587db697170b3da2790d0bf28a1e91e9
kvsathish <karthiksathish2025@u.northwestern.edu>
2023-12-04 19:10:09 -0600

    more r scripts

---
+++ 3_multivariate_analyses.R
@@ -0,0 +1,12 @@
+# Multivariate Analyses ----
+# Stat 301-1
+
+## load packages ----
+library(tidyverse)
+library(palmerpenguins)
+library(knitr)
+
+## read in data ----
+allgames_stats <- read_csv("data/allgames_stats.csv")
+
+allgames_stats
---
+++ 4_more_notes.R
@@ -0,0 +1,35 @@
+# More Notes ----
+# Stat 301-1
+
+## extra notes for final project ----
+
+# look at how production changes in the postseason (still producing in a high rate)
+
+# quality of team making the playoff
+
+# analyze pts and stats uniquely before the EDA - see if they correlate with each other in any way
+# make sure to weigh the variables uniquely
+# wins should matter - incorporate it somehow (if team crumbles when they're playing bad)
+# analyze games won to games lost
+# standardize scores by using z score - avg pts for a year is 18 (divide by and find spread)
+# standardize it - do it for every year
+# z scores for each year should be standardized 
+# score metric will likely look the same in each year - play at least 5 min per game to be considered
+# relative to the rest of league, how much better are they?
+# get season averages and standard deviation for calculations (be careful about players - make 10 min minimum and at least 10 games)
+# 
+
+# do univariate analysis of each variable for the score metric separately - compare to win totals and see if that team is in playoff contention (how good the team was)
+# maybe join to see playoff vs. nonplayoff teams
+# decide on the score metric and analyze relationship with the other variables
+# analyze compared to wins/losses
+# break down the playoff games too to evaluate postseason
+# expected to have appendix when there's an abundance of analyses - put the not so cool one in the appendix section
+# make a good narrative about score and analyzing each component - try the launchpads and ones that bear fruit 
+# compare scores to other variables and go from there 
+# see how score correlates to turnovers (how adjustment changes score)
+# compare to the NBA game score metric
+# multivariate - (when, score, turnovers)
+# talk about how u came up with score (explain why u picked them) - these are the most important stats that contribute to winning
+# five year phases of the career (all started young - after 5 years of experience)
+# have a good number of bivariate/multivariate analyses (splitting season/postseason wins / splitting players into phases)
